21e6983a907498cd951263348d98f8659c68a24d
jaimeav <jalvaradol@alumnos.unex.es>
2020-11-23 20:56:13 +0000

    Testing

--- src/test/java/com/example/StepsApiServiceImplTest.java
+++ src/test/java/com/example/StepsApiServiceImplTest.java
@@ -6,21 +6,42 @@ package com.example;
 
 import javax.ws.rs.core.Application;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import io.swagger.api.impl.StepsApiServiceImpl;
 
 /**
  * Unit test for simple App.
  */
 @RunWith(JUnit4.class)
-public class StepsApiServiceImplTest {
+public class StepsApiServiceImplTest extends JerseyTest {
+
+     @Override
+    public Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        return new ResourceConfig(StepsApiServiceImpl.class);
+    }
 
     @Test
     public void testApp() {
         assertTrue(true);
     }
+
+    /*@Test
+    public void testGetIt() {
+        final String responseMsg = target().path("steps/20").request().get(String.class);
+
+        assertEquals("400 pasos", responseMsg);
+    }*/
 
      @Test
     public void testApp2() {
